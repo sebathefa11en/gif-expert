4f4fefc2a7ae75dcc778506531540e90c00b4c80
Sebastian Gonzalez <s.gonzalez.calquin@gmail.com>
2022-05-21 19:30:47 -0400

    ADD : componente para agregar elementos en la lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
+import AddCategory from './components/AddCategory';
 
 const GifExpertApp = () => {
 
   //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
   const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-  const handleAdd = () => {
+  /* const handleAdd = () => {
     //setCategories([...categories, 'Hunter X Hunter']);
     setCategories(cats => [...cats, 'Hunter X Hunter']);
-  }
+  } */
+
+
 
   return (
     <>
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories={ setCategories }/>
         <hr />
         <ol>
           {
@@ -21,7 +25,7 @@ const GifExpertApp = () => {
             })
           }
         </ol>
-        <button onClick={handleAdd}>Agregar</button>
+        <button>Agregar</button>
     </>
   )
 }
