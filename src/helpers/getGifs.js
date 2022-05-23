b90f3f69bfc563ec9746e187c3e88b522a473cee
Sebastian Gonzalez <s.gonzalez.calquin@gmail.com>
2022-05-23 08:01:27 -0400

    ADD : build

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=0hsYicxzfhw4n3J55FivCfRoeFeSRIvj&q=${encodeURI(category)}&limit=20&offset=0&rating=g&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_GIF}q=${encodeURI(category)}&limit=20&offset=0&rating=g&lang=en`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
